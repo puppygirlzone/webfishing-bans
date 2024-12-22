e9ccc7969b2fe8d740594a4287a3945325d8207b
Ahlo <153831898+puppygirlzone@users.noreply.github.com>
2024-12-22 19:35:34 +1030

    Updated HTML generator to utilize Jinja2 templating for improved layout and modularity
      * Refactored `get_steam_id_finder_html` function to use Jinja2 syntax
      * Introduced template system with a new 'shame.html' template file
      * Modified main script to load and utilize the new Jinja2 templates
      * Exported generated HTML to a file using the `FileSystemLoader` for easier management

--- html.py
+++ html.py
@@ -1,23 +1,28 @@
 import json
 from pathlib import Path
 
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 def get_steam_id_finder_html(steam_id: int | str) -> str:
-    return (f"<a href='https://www.steamidfinder.com/lookup/{steam_id}/'>"
-            f"<img src='https://www.steamidfinder.com/signature/{steam_id}.png'>"
-            "</a>")
-
+    return (f"{{'https://www.steamidfinder.com/lookup/{steam_id}/'>"
+            f"<img src='https://www.steamidfinder.com/signature/{steam_id}_png'>")
 
 def gen_payload(ban_list: list[int | str]) -> str:
-    return "\n".join([get_steam_id_finder_html(steam_id) for steam_id in ban_list])
+    return " ".join([get_steam_id_finder_html(steam_id) for steam_id in ban_list])
     
-
 if __name__ == "__main__":
     with Path("LobbyLifeguard.json").open() as f:
         raw = json.load(f)
-        ban_list = raw["banlist"].split(", ")
+        ban_list = [int(ban_id.strip()) for ban_id in raw["banlist"].split(", ")]
 
-    # TODO - Change this so it has some actual CSS and other content. Maybe use jinja?
+    # Load template
+    file_loader = FileSystemLoader('templates')
+    env = Environment(loader=file_loader, autoescape=select_autoescape(['html', 'xml']))
+    template = env.get_template('shame.html')
+
+    # Render template with data
+    output = template.render(ban_list=ban_list)
+    
+    # Write to file
     with Path("build/shame.html").open("w") as f:
-        payload = gen_payload(ban_list)
-        f.write(payload)
+        f.write(output)
